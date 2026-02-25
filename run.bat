075726d050571014339bfffcb863a3203074ebe8
Dhi5a <dhiabondka@gmail.com>
2026-02-25 17:19:09 +0100

    Implementation captcha et mailing

--- run.bat
+++ run.bat
@@ -1,27 +1,16 @@
 @echo off
-REM Script pour compiler et lancer l'application Green Ledger
+REM Script pour lancer l'application Green Ledger
 
-echo [INFO] Compilation du projet...
-javac -cp "src/main/java" -d target/classes -encoding UTF-8 ^
-    src/main/java/org/GreenLedger/*.java ^
-    src/main/java/Controllers/*.java ^
-    src/main/java/Services/*.java ^
-    src/main/java/dao/*.java ^
-    src/main/java/Models/*.java ^
-    src/main/java/DataBase/*.java ^
-    src/main/java/Utils/*.java
+setlocal enabledelayedexpansion
+set JAVAFXDIR=%USERPROFILE%\.m2\repository\org\openjfx
 
-if %errorlevel% neq 0 (
-    echo [ERR] Erreur de compilation !
-    pause
-    exit /b 1
-)
+echo [INFO] Application launched...
 
-echo [OK] Compilation reussie !
-echo [INFO] Demarrage de l'application...
-
-REM Lancer l'application
-java -cp "target/classes;src/main/java" org.GreenLedger.MainFX
+java ^
+  --module-path "%JAVAFXDIR%\javafx-controls\20.0.2\javafx-controls-20.0.2-win.jar;%JAVAFXDIR%\javafx-graphics\20.0.2\javafx-graphics-20.0.2-win.jar;%JAVAFXDIR%\javafx-base\20.0.2\javafx-base-20.0.2-win.jar;%JAVAFXDIR%\javafx-fxml\20.0.2\javafx-fxml-20.0.2-win.jar;%JAVAFXDIR%\javafx-web\20.0.2\javafx-web-20.0.2-win.jar" ^
+  --add-modules javafx.controls,javafx.fxml,javafx.web ^
+  -cp "target\classes;%USERPROFILE%\.m2\repository\mysql\mysql-connector-java\8.0.26\mysql-connector-java-8.0.26.jar;%USERPROFILE%\.m2\repository\org\mindrot\jbcrypt\0.4\jbcrypt-0.4.jar;%USERPROFILE%\.m2\repository\com\sun\mail\jakarta.mail\2.0.1\jakarta.mail-2.0.1.jar;%USERPROFILE%\.m2\repository\com\sun\activation\jakarta.activation\2.0.1\jakarta.activation-2.0.1.jar;%USERPROFILE%\.m2\repository\org\apache\pdfbox\pdfbox\2.0.29\pdfbox-2.0.29.jar;%USERPROFILE%\.m2\repository\org\apache\pdfbox\fontbox\2.0.29\fontbox-2.0.29.jar;%USERPROFILE%\.m2\repository\commons-logging\commons-logging\1.2\commons-logging-1.2.jar;%USERPROFILE%\.m2\repository\org\apache\opennlp\opennlp-tools\2.3.3\opennlp-tools-2.3.3.jar;%USERPROFILE%\.m2\repository\org\slf4j\slf4j-api\1.7.36\slf4j-api-1.7.36.jar;%USERPROFILE%\.m2\repository\org\slf4j\slf4j-simple\1.7.36\slf4j-simple-1.7.36.jar;%USERPROFILE%\.m2\repository\org\apache\httpcomponents\client5\httpclient5\5.2.1\httpclient5-5.2.1.jar;%USERPROFILE%\.m2\repository\org\apache\httpcomponents\core5\httpcore5\5.2\httpcore5-5.2.jar;%USERPROFILE%\.m2\repository\org\apache\httpcomponents\core5\httpcore5-h2\5.2\httpcore5-h2-5.2.jar;%USERPROFILE%\.m2\repository\com\google\code\gson\gson\2.10.1\gson-2.10.1.jar" ^
+  org.GreenLedger.MainFX
 
 pause
 
